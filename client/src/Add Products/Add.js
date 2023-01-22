832a500f8797f96d4c960eb271d036b7b65908d9
aygnn <6yary5x@code.edu.az>
2023-01-23 02:23:10 +0400

    practicee

--- client/src/Add Products/Add.js
+++ client/src/Add Products/Add.js
@@ -1,23 +1,23 @@
 import React, { useState } from 'react'
 import './Add.css';
-import { Formik, Form, Field } from 'formik';
-
+import { Formik, Form, Field,ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { TiTick } from 'react-icons/ti';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import axios from 'axios';
+
+// const AddSchema=
 
 
 export default function Add() {
   const[products,setProducts]=useState([])
   const[product,setProduct]=useState([])
 
-
-  // axios.post('http://localhost:2000/products')
-  // .then(res=>console.log(res.data))
-
-
-
+  
+  
+  
   return (
     
     <div>
@@ -27,21 +27,37 @@ export default function Add() {
         price:'',
         stock:0
       }}
-      onSubmit={(values)=>{
-      // setProduct(values)
-   
-  
-        console.log(product);
+      validationSchema={Yup.object({
+        name:Yup.string().required('Product name is required'),
+        price:Yup.string().required('Product price is required'),
+        stock:Yup.number().required('Product stock is required')
+      
+      })}
+
+      
+      onSubmit={async(values)=>{
+        
+       
+         await axios.post('http://localhost:2000/products',{
+          name:values.name,
+          price:values.price,
+          stock:values.stock
+         }).then(res=>console.log(res.data))
+       
+          
       } }
       >
-        {({values})=>(
+        {({errors,touched})=>(
        <Form>
 
-         <Field as={TextField}  id="outlined-basic" name='name' label="Product Name" variant="outlined" /><br/>
+         <Field as={TextField}  id="outlined-basic" name='name' label="Product Name" variant="outlined" style={errors.name && touched.name && {borderColor:'red'}}/><br/>
+         {/* {errors.name && touched.name && (<div style={{color:'red'}}>{errors.name}</div>)} */}
+         <ErrorMessage className='error' name='name'/>
+
          <Field as={TextField}  id="outlined-basic" name='price' label="Product Price" variant="outlined" /><br/>
          <Field as={TextField}  id="outlined-basic" name='stock' label="Stock number" variant="outlined" /><br/>
         
-         <Stack spacing={2} direction="row">
+         <Stack className='stack' spacing={2} direction="row">
    
       <Button type='submit' variant="contained">Contained</Button>
     
